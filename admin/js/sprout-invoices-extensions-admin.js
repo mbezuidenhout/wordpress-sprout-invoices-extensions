59b40692050a654387b23f835d1b71be28fa4fa8
Marius Bezuidenhout <marius.bezuidenhout@gmail.com>
2019-08-25 10:05:01 +0200

    5d6107ab2a1fc08193b41e7e Hide tax2

--- admin/js/sprout-invoices-extensions-admin.js
+++ admin/js/sprout-invoices-extensions-admin.js
@@ -2,19 +2,10 @@
 	'use strict';
 
 	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
+	 $(function() {
+
+	 });
+
 	 * When the window is loaded:
 	 *
 	 * $( window ).load(function() {
